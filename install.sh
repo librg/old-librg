1fba24cf9eeaa16d6b17afee35669ea51ba4461e
Vladislav Gritsenko <vladgritsenko@gmail.com>
2017-01-21 15:27:07 +0200

    cleanup

--- install.sh
+++
@@ -1,5 +0,0 @@
-cd vendor/RakNet && git submodule init && git submodule update;
-cd vendor/libuv && git submodule init && git submodule update;
-cd vendor/squirrel-lang && git submodule init && git submodule;
-
-echo "Done.";
