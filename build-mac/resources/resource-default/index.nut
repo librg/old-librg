25e1542a422dc5317eb5536702c8ed3e1f9e20f9
Vladislav Gritsenko <vladgritsenko@gmail.com>
2017-02-18 16:13:44 +0200

    added demo of advanced parameter registration

--- build-mac/resources/resource-default/index.nut
+++ build-mac/resources/resource-default/index.nut
@@ -1,2 +1,5 @@
 print("index.nut at root");
 print(foo() + abc());
+
+
+test2("hello", 15, 242.24);
--- server/Scripting/System.hpp
+++ server/Scripting/System.hpp
@@ -14,9 +14,15 @@ namespace System
         Core::Log("its a test message: %s", message);
     }
 
+    inline static void test2(const char* message, int number, float otherNumber)
+    {
+        Core::Log("its a test message 2 with: %s, %d, %f", message, number, otherNumber);
+    }
+
     inline static void Install(HSQUIRRELVM vm)
     {
         RootTable(vm).Func("test", &test);
+        RootTable(vm).Func("test2", &test2);
     }
 }
 
