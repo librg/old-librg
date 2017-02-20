ea599e0893b55706f72663eabd3c7eb3cdcf0585
Dominik Madarász <madaraszd@mail.ru>
2017-02-20 20:28:07 +0100

    Event execution changes.

--- server/Scripting/Natives/Event.hpp
+++ server/Scripting/Natives/Event.hpp
@@ -9,7 +9,33 @@ namespace Scripting {
 
 namespace Event
 {
+    struct EventData
+    {
+        Function* callback;
+        std::string params;
+    };
+
     static std::unordered_map<std::string, std::vector<Function>> eventHandlers;
+
+    inline static void private_eventCleanup(uv_async_t* req)
+    {
+        auto data = (EventData*)req->data;
+        delete data->callback;
+        delete data;
+        uv_close((uv_handle_t*)req, NULL);
+    }
+
+    inline static void private_eventCallback(uv_async_t* req)
+    {
+        auto data = (EventData*)req->data;
+        data->callback->Execute(data->params);
+
+        uv_async_t* async = new uv_async_t;
+        uv_async_init(uv_default_loop(), async, private_eventCleanup);
+
+        async->data = req->data;
+        uv_async_send(async);
+    }
 
     inline static void eventAddHandler(const char* handlerName, Function callback)
     {
@@ -30,7 +56,13 @@ namespace Event
         {
             for(auto handler : eventHandlers[handlerName])
             {
-                handler.Execute(params);
+                uv_async_t* async = new uv_async_t;
+                auto callback = new Function(handler);
+                std::string cparams = params;
+                auto data = new EventData { callback, cparams };
+                uv_async_init(uv_default_loop(), async, private_eventCallback);
+                async->data = (void*)data;
+                uv_async_send(async);
             }
         }
     }
