970c90cd2e8c03a35ea6e30ff8e070e1fcccc6b3
Vladislav Gritsenko <vladgritsenko@gmail.com>
2017-03-01 21:41:20 +0200

    some updated in build version

--- include/librg/Shared/BuildVersion.h
+++ include/librg/Shared/BuildVersion.h
@@ -1,8 +1,22 @@
-#ifndef __build_version
-#define __build_version
+/**
+ * Random number setting current game platform id
+ * to prevent connections from different games/mods, in future
+ */
+#ifndef NETWORK_PLATFORM_ID
+#define NETWORK_PLATFORM_ID 1
+#endif
 
-#define NETWORK_PLATFORM_ID          121 // random number setting current game platform id, to prevent connections from different games/mods, in future
-#define NETWORK_PROTOCOL_VERSION     1   // version of protocol, should be changed if major, incompatible changes are introduced
-#define NETWORK_BUILD_VERSION        1   // version of build. server owner can decide, should clients should be refused or allowed joining the game
+/**
+ * Version of protocol, should be changed if major, incompatible changes are introduced
+ */
+#ifndef NETWORK_PROTOCOL_VERSION
+#define NETWORK_PROTOCOL_VERSION 1
+#endif
 
-#endif // __build_version
+/**
+ * version of build. server owner can decide,
+ * should clients should be refused or allowed joining the game
+ */
+#ifndef NETWORK_PROTOCOL_VERSION
+#define NETWORK_BUILD_VERSION 1
+#endif
