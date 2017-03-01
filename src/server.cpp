9759488c39c34d035a84163170a20950706cb5d1
Vladislav Gritsenko <vladgritsenko@gmail.com>
2017-03-01 21:53:48 +0200

    cleaned up stuff

--- src/server.cpp
+++ src/server.cpp
@@ -1,188 +1,179 @@
-// #ifdef WIN32
-// #pragma comment(lib, "IPHLPAPI.lib")
-// #pragma comment(lib, "psapi.lib")
-// #pragma comment(lib, "shell32.lib")
-// #pragma comment(lib, "advapi32.lib")
-// #pragma comment(lib, "ws2_32.lib")
-// #pragma comment(lib, "userenv.lib")
-// #endif
-
 #include <uv.h>
 #include <stdinc.h>
 #include <clocale>
-
 #include <Core.h>
 
-uint64_t counter = 0;
+namespace Server {
 
-/**
- * Main loop ticker
- * @param handle [description]
- */
-void timed_loop(uv_timer_t* handle)
-{
-    Server::Core::Instance()->Tick(counter++);
-}
+    uint64_t counter = 0;
 
-/**
- * Main loop ticker
- * @param handle [description]
- */
-void idle_loop(uv_timer_t* handle)
-{
-    Server::Core::Instance()->Idle();
-}
-
-
-/**
- * System signal hanlder
- * @param req
- * @param signum
- */
-void signal_handler(uv_signal_t *req, int signum)
-{
-    // stop for ctrl+C
-    if (signum == 2) {
-        // Server::Core::Log("\nExiting!");
-        uv_stop(uv_default_loop());
+    /**
+     * Main loop ticker
+     * @param handle [description]
+     */
+    void timed_loop(uv_timer_t* handle)
+    {
+        Server::Core::Instance()->Tick(counter++);
     }
 
-    uv_signal_stop(req);
-}
-
-/**
- * Alloc callback for allocating input memory
- * @param handle         tty handle
- * @param suggested_size suggensted size by uv (65536 in most cases)
- * @param buf            buffer, where data will be written to, and read from by us
- */
-static void tty_alloc(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf)
-{
-    buf->base = new char[1024];
-    buf->len = 1024;
-}
-
-/**
- * On user console message
- * @param stream tty handle
- * @param nread  size of string
- * @param buf    buffer with data
- */
-void on_console_message(uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf)
-{
-    buf->base[nread] = '\0';
-    Server::Core::Instance()->OnInput(buf->base);
-}
-#include <sqrat.h>
-using namespace Server;
-
-void event_system_cpp_to_sq() {
-    int amount = 666;
-    std::string name = "Vlad";
-
-    Event::Manager::Instance()->Dispatch("onDeveloperDrinksVodka", EVENT_PARAM_SQ([=](HSQUIRRELVM vm){
-        auto array = new Sqrat::Array(vm);
-        array->Append(amount);
-        array->Append(name);
-        return array;
-    }));
-}
-
-// TODO(zaklaus): Move these test blocks somewhere, main.cpp just gets bloated by this.
-struct VodkaEvent
-{
-    std::string amount;
-};
-
-void* VodkaEventResponse(const void* data, Sqrat::Array* array) {
-    if(array!=0) {
-        auto vodka = new VodkaEvent;
-        std::string amount = array->Pop().Cast<char*>();
-        vodka->amount = amount;
-        return vodka;
+    /**
+     * Main loop ticker
+     * @param handle [description]
+     */
+    void idle_loop(uv_timer_t* handle)
+    {
+        Server::Core::Instance()->Idle();
     }
-    return (void*)data;
-}
 
-void event_system_test() {
-    size_t handlerId = Event::Manager::Instance()->AddListener("onVodkaTooWeak", [](const void* event, void* /* blob */){
-        const VodkaEvent* vodka = (VodkaEvent*)event;
-        Core::Log("We need %s of vodka!", vodka->amount.c_str());
-    }, VodkaEventResponse);
+    /**
+     * System signal hanlder
+     * @param req
+     * @param signum
+     */
+    void signal_handler(uv_signal_t *req, int signum)
+    {
+        // stop for ctrl+C
+        if (signum == 2) {
+            // Server::Core::Log("\nExiting!");
+            uv_stop(uv_default_loop());
+        }
 
-    Event::Manager::Instance()->Dispatch("onVodkaTooWeak", EVENT_PARAM_CPP(new VodkaEvent{ "a lot" }));
+        uv_signal_stop(req);
+    }
 
-    Event::Manager::Instance()->AddListener("onTestMessageRequested", [](const void* /* event */, void* /* blob */){
-        Core::Log("This is a test message!");
-    }, GenericNoResponse);
-}
+    /**
+     * Alloc callback for allocating input memory
+     * @param handle         tty handle
+     * @param suggested_size suggensted size by uv (65536 in most cases)
+     * @param buf            buffer, where data will be written to, and read from by us
+     */
+    static void tty_alloc(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf)
+    {
+        buf->base = new char[1024];
+        buf->len = 1024;
+    }
 
-/**
- * Starts the server!
- * @param  argc
- * @param  argv
- * @return exit code
- */
-int Start(int argc, char * argv[]) {
-    uv_timer_t idler;
-    uv_timer_t timer_req;
-    uv_tty_t tty;
+    /**
+     * On user console message
+     * @param stream tty handle
+     * @param nread  size of string
+     * @param buf    buffer with data
+     */
+    void on_console_message(uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf)
+    {
+        buf->base[nread] = '\0';
+        Server::Core::Instance()->OnInput(buf->base);
+    }
+
+    /**
+     * Starts the server!
+     * @param  argc
+     * @param  argv
+     * @return exit code
+     */
+    int Start(int argc, char * argv[]) {
+        uv_timer_t idler;
+        uv_timer_t timer_req;
+        uv_tty_t tty;
 
 #ifdef WIN32
-    // Set our locale to the C locale, as Unicode output only functions in this locale
-    std::setlocale(LC_ALL, "C");
-    SetConsoleOutputCP(CP_UTF8);
+        // Set our locale to the C locale, as Unicode output only functions in this locale
+        std::setlocale(LC_ALL, "C");
+        SetConsoleOutputCP(CP_UTF8);
 #endif
-    // Server::Core::Log("\033[2J\033[H\033[1B\033[1C\033[42;37m%s");
+        // Server::Core::Log("\033[2J\033[H\033[1B\033[1C\033[42;37m%s");
 
-    // create and initialize
-    Server::Core::Instance();
+        // create and initialize
+        Server::Core::Instance();
 
+        // define main timed loop (network send)
+        // start after 1 sec, each 15 ms
+        uv_timer_init(uv_default_loop(), &timer_req);
+        uv_timer_start(&timer_req, timed_loop, 1000, 16);
 
-    // define main timed loop (network send)
-    // start after 1 sec, each 15 ms
-    uv_timer_init(uv_default_loop(), &timer_req);
-    uv_timer_start(&timer_req, timed_loop, 1000, 16);
+        uv_timer_init(uv_default_loop(), &idler);
+        uv_timer_start(&idler, idle_loop, 0, 1);
 
-    uv_timer_init(uv_default_loop(), &idler);
-    uv_timer_start(&idler, idle_loop, 0, 1);
+        // define main idle loop (network receieve)
+        // uv_idle_init(uv_default_loop(), &idler);
+        // uv_idle_start(&idler, idle_loop);
 
-    // define main idle loop (network receieve)
-    // uv_idle_init(uv_default_loop(), &idler);
-    // uv_idle_start(&idler, idle_loop);
+        // singal handling
+        uv_signal_t sig;
+        uv_signal_init(uv_default_loop(), &sig);
+        uv_signal_start(&sig, signal_handler, SIGINT);
 
-    // singal handling
-    uv_signal_t sig;
-    uv_signal_init(uv_default_loop(), &sig);
-    uv_signal_start(&sig, signal_handler, SIGINT);
+        // terminal window
+        uv_tty_init(uv_default_loop(), &tty, 0, 1);
+        uv_tty_set_mode(&tty, UV_TTY_MODE_NORMAL);
 
-    // terminal window
-    uv_tty_init(uv_default_loop(), &tty, 0, 1);
-    uv_tty_set_mode(&tty, UV_TTY_MODE_NORMAL);
+        // setup reading callback
+        uv_read_start((uv_stream_t*)&tty, tty_alloc, on_console_message);
 
-    // setup reading callback
-    uv_read_start((uv_stream_t*)&tty, tty_alloc, on_console_message);
+        // if (uv_tty_get_winsize(&tty, &width, &height)) {
+        //     fprintf(stderr, "Could not get TTY information\n");
+        //     uv_tty_reset_mode();
+        //     return 1;
+        // }
 
-    // if (uv_tty_get_winsize(&tty, &width, &height)) {
-    //     fprintf(stderr, "Could not get TTY information\n");
-    //     uv_tty_reset_mode();
-    //     return 1;
-    // }
+        // fprintf(stderr, "Width %d, height %d\n", width, height);
+        // uv_timer_init(uv_default_loop(), &tick);
+        // uv_timer_start(&tick, update, 200, 200);
 
-    // fprintf(stderr, "Width %d, height %d\n", width, height);
-    // uv_timer_init(uv_default_loop(), &tick);
-    // uv_timer_start(&tick, update, 200, 200);
+        // starting loop
+        uv_run(uv_default_loop(), UV_RUN_DEFAULT);
 
-    event_system_test();
+        // after work is done, closing loop
+        uv_loop_close(uv_default_loop());
 
-    // starting loop
-    uv_run(uv_default_loop(), UV_RUN_DEFAULT);
-
-    // after work is done, closing loop
-    uv_loop_close(uv_default_loop());
-
-    return 0;
+        return 0;
+    }
 }
+
+
+// void event_system_cpp_to_sq() {
+//     int amount = 666;
+//     std::string name = "Vlad";
+
+//     Event::Manager::Instance()->Dispatch("onDeveloperDrinksVodka", EVENT_PARAM_SQ([=](HSQUIRRELVM vm){
+//         auto array = new Sqrat::Array(vm);
+//         array->Append(amount);
+//         array->Append(name);
+//         return array;
+//     }));
+// }
+
+// // TODO(zaklaus): Move these test blocks somewhere, main.cpp just gets bloated by this.
+// struct VodkaEvent
+// {
+//     std::string amount;
+// };
+
+// void* VodkaEventResponse(const void* data, Sqrat::Array* array) {
+//     if(array!=0) {
+//         auto vodka = new VodkaEvent;
+//         std::string amount = array->Pop().Cast<char*>();
+//         vodka->amount = amount;
+//         return vodka;
+//     }
+//     return (void*)data;
+// }
+
+// void event_system_test() {
+//     size_t handlerId = Event::Manager::Instance()->AddListener("onVodkaTooWeak", [](const void* event, void* /* blob */){
+//         const VodkaEvent* vodka = (VodkaEvent*)event;
+//         Core::Log("We need %s of vodka!", vodka->amount.c_str());
+//     }, VodkaEventResponse);
+
+//     Event::Manager::Instance()->Dispatch("onVodkaTooWeak", EVENT_PARAM_CPP(new VodkaEvent{ "a lot" }));
+
+//     Event::Manager::Instance()->AddListener("onTestMessageRequested", [](const void* /* event */, void* /* blob */){
+//         Core::Log("This is a test message!");
+//     }, GenericNoResponse);
+// }
+
+// event_system_test();
+
 
 
 
