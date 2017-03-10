dc352967a6ab65ab4392db3e929f2db7e9a538ae
Dominik Madarasz <madaraszd@mail.ru>
2017-03-10 09:15:26 +0100

    Scripting API changes.

--- include/librg/Scripting/Natives/Event.hpp
+++ include/librg/Scripting/Natives/Event.hpp
@@ -9,11 +9,11 @@ namespace Scripting {
 namespace Event
 {
     /**
-     * Public API method for adding event handler. Multiple event handlers per event name are supported.
-     * @param eventName Event name under which the handler should be registered.
+     * Public API method for adding event listener. Multiple event handlers per event name are supported.
+     * @param eventName Event name under which the listener should be registered.
      * @param callback    Squirrel wrapped function object.
      */
-    inline static void eventAddHandler(const char* eventName, Function callback)
+    inline static void eventAddListener(const char* eventName, Function callback)
     {
         size_t handlerId = Server::Event::Manager::Instance()->AddListener(eventName, [](const void *event, void* blob){
             auto array = (Sqrat::Array*)event;
@@ -25,11 +25,11 @@ namespace Event
     }
 
     /**
-     * Public API method for triggering server event. This method passes handler arguments via Squirrel array. This method calls all registered handlers under specified event name.
+     * Public API method for dispatching an event. This method passes handler arguments via Squirrel array. This method calls all registered handlers under specified event name.
      * @param eventName   Name of the event to call.
      * @param params      Array of values.
      */
-    inline static void eventServerTrigger(const char* eventName, Array array)
+    inline static void eventDispatch(const char* eventName, Array array)
     {
         Server::Event::Manager::Instance()->Dispatch(eventName, EVENT_PARAM_SQ_INTERNAL(new Array(array)));
     }
@@ -40,8 +40,8 @@ namespace Event
      */
     inline static void Install(Table& table)
     {
-        table.Func("eventAddHandler",    &eventAddHandler);
-        table.Func("eventServerTrigger", &eventServerTrigger);
+        table.Func("eventAddListener",    &eventAddListener);
+        table.Func("eventDispatch", &eventDispatch);
     }
 }
 
