d4e8d2a4e293326e131c9d084448546e957e4b71
Dominik Madarász <madaraszd@mail.ru>
2017-03-27 17:15:40 +0200

    slightly changed the tests and made them more real.

--- test/perf/streamer.hpp
+++ test/perf/streamer.hpp
@@ -14,7 +14,7 @@ namespace performance
             entity.assign<librg::streamable_t>(vectorial::vec3f(250));
             entity.assign<librg::transform_t>();
 
-            test("insertion of 48k entities", [entity]() {
+            test("insertion and deletion of 48k entities", [entity]() {
                 for (int i = 0; i < 48000; i++) {
                     auto enemy = librg::entities->create();
                     enemy.assign<librg::streamable_t>(vectorial::vec3f(300));
@@ -23,8 +23,19 @@ namespace performance
                     enemy.assign<librg::transform_t>(ft);
 
                     librg::streamer::insert(enemy);
+                    librg::streamer::clear();
                 }
             });
+
+            for (int i = 0; i < 48000; i++) {
+                auto enemy = librg::entities->create();
+                enemy.assign<librg::streamable_t>(vectorial::vec3f(300));
+                auto ft = librg::transform_t();
+                ft.position = vectorial::vec3f(i,i,10);
+                enemy.assign<librg::transform_t>(ft);
+
+                librg::streamer::insert(enemy);
+            }
 
             test("query of 48k entities with 250 units query range", [entity]() {
                 auto queue = librg::streamer::query(entity);
