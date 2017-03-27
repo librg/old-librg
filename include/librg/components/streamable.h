bd7d7ca1c0768e16a2f3d255f2a07d53cad1eaa7
Dominik Madarász <madaraszd@mail.ru>
2017-03-27 08:51:15 +0200

    cleanup m8

--- include/librg/components/streamable.h
+++ include/librg/components/streamable.h
@@ -9,6 +9,7 @@ namespace librg
     struct streamable_t {
         streamable_t(): queryRange(250), ignoredEntities(), type(0) {}
         streamable_t(vec3f queryRange): queryRange(queryRange), ignoredEntities(), type(0) {}
+        streamable_t(float x, float y, float z): queryRange(vectorial::vec3f(x,y,z)), ignoredEntities(), type(0) {}
         streamable_t(vec3f queryRange, uint8_t type): queryRange(queryRange), ignoredEntities(), type(type) {}
         vec3f queryRange;
         std::vector<Entity> ignoredEntities;
