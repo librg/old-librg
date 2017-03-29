807b874d58d973083fe8008ef069e7670eb79193
Vladislav Gritsenko <vladgritsenko@gmail.com>
2017-03-29 22:51:26 +0300

    moved stuff

--- src/core/client.cpp
+++ src/core/client.cpp
@@ -1,4 +1,4 @@
-﻿#include <librg/core/client.h>
+#include <librg/core/client.h>
 
 using namespace librg;
 
@@ -38,6 +38,7 @@ void c_initialize()
 
     uv_timer_init(uv_default_loop(), idler);
     uv_timer_start(idler, c_idle_loop, 0, 1);
+
     initialized = true;
 }
 
