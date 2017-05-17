b6c613f128db79176e0b4779448718fc9448d894
Vladislav Gritsenko <vladgritsenko@gmail.com>
2017-05-17 22:17:13 +0300

    removed http code

--- src/http/add_header.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright ReGuider Team, 2016-2017
-//
-#include <librg/http/client.h>
-
-void librg::network::http::add_header(request_t *req, std::string head, std::string text)
-{
-    std::string line = head + ":" + text + "\r\n";
-    req->headers.append(line);
-}
--- src/http/set_data.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright ReGuider Team, 2016-2017
-//
-#include <librg/http/client.h>
-
-void librg::network::http::set_data(request_t *req, void* data, uint64_t length)
-{
-    req->data = data;
-    req->length = length;
-}
